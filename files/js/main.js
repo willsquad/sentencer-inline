375beaa00482cd023ca2075f4839c36d84a2f27e
Ashil John <willsquad.aj@gmail.com>
2018-07-10 15:13:01 +0530

    v2_suggestions_inline

---
+++ files/js/main.js
@@ -0,0 +1,69 @@
+$(document).ready(function() {
+    /* document.querySelector("div[contenteditable]").addEventListener("paste", function(e) {
+        e.preventDefault();
+        var text = (e.originalEvent || e).clipboardData.getData('text/plain');
+        document.execCommand("insertHTML", false, text);
+    }); */
+
+        /* Get Selection  */
+        $('#textarea_input').on('mouseup', function (e) {
+
+            var editor_input_width = document.getElementById("textarea_input").offsetWidth;
+    
+            var selText = "";
+            var cursor_x = e.clientX;
+            var cursor_y = e.clientY - 125;
+    
+            if (window.getSelection) { // all browsers, except IE before version 9
+    
+                var selRange = window.getSelection();
+                var selText = selRange.toString();
+    
+                /*create an element before the selection to get the position*/
+                var oRange = selRange.getRangeAt(0); //get the text range
+                var oRect = oRange.getBoundingClientRect();
+    
+                var cursor_y = oRect.top - 210;
+                var cursor_x = oRect.left;
+    
+                if (cursor_x > editor_input_width / 1.05) {
+                    var cursor_x = editor_input_width / 1.05;
+                }
+    
+                if (selText !== "") {
+                    //alert (selText);
+                    $('.inline_suggestions').addClass('active');
+                    $('.inline_suggestions').css({
+                        "top": "" + cursor_y + "px",
+                        "left": "" + cursor_x + "px"
+                    });
+
+                    $('#selected_word').html(selText);
+    
+                } else {
+                    $('.inline_suggestions').removeClass('active');
+                }
+            } else {
+                if (document.selection.createRange) { // Internet Explorer
+                    var range = document.selection.createRange();
+                    var selText = range.text;
+                }
+    
+                if (selText !== "") {
+                    //alert (selText);
+                    $('.inline_suggestions').toggleClass('active');
+                    $('.inline_suggestions').css({
+                        "top": "" + cursor_y + "px",
+                        "left": "" + cursor_x + "px"
+                    });
+
+                    $('#selected_word').html(selText);
+    
+                } else {
+    
+                }
+            }
+    
+        });
+        /* Get Selection */
+});
